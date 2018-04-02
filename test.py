4cd1e7983d4ae1fe19877d28b4f8950c3a788cf5
Kanishka Bhat <kanishkabhat7@gmail.com>
2018-04-02 15:56:56 +0530

    'update'

---
+++ test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+port = int(os.getenv("PORT"))
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    #port = 1245
+    app.run(host='0.0.0.0',port=port)
